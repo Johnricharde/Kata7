0bd9408512d11cf3907cd789a337cdd1f584d279
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-14 16:48:51 +0100

    Add project files.

---
+++ Kata7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static int[] SortArray(int[] array)
+        {
+            int[] oddNumbers = array
+                .Where(num => num % 2 != 0)
+                .OrderBy(oddNum => oddNum)
+                .ToArray();
+
+            for (int i = 0, j = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    array[i] = oddNumbers[j];
+                    j++;
+                }
+            }
+
+            return array;
+        }
+    }
+}
